3193af95c5616481ddae788b5a08807a0ebf652b
Ananas <mrs.anna.noga@gmail.com>
2023-01-21 21:41:58 +0000

    timeblock done

--- assets/script.js
+++ assets/script.js
@@ -8,3 +8,16 @@ $("#currentDay").text(now.format("dddd, D MMMM YYYY, h:mm:ss a"));
 
 setInterval(updateNow);
 
+// create timeblock via javascript and jquery
+
+// var timeblockContainer = $(".container");
+// var div1 = $("<div>");
+// var divInDiv = $("<div>");
+// var textArea1 = $("<textarea>");
+// var button1 = $("<button>");
+
+// var row1 = timeblockContainer.append(div1);
+// div1.append(divInDiv, textArea1, button1);
+// row1.addId("9AM");
+// row1.addClass("row", "time-div");
+
