3260d5b1ebc1d2eecfa7932c99348555f3ebd179
Ananas <mrs.anna.noga@gmail.com>
2023-01-22 21:38:21 +0000

    saved data loads when page is refreshed or is loading

--- assets/script.js
+++ assets/script.js
@@ -8,19 +8,51 @@ $("#currentDay").text(now.format("dddd, D MMMM YYYY, h:mm:ss a"));
 
 setInterval(updateNow);
 
-//event delegation to save button to save text in local storage
+//set moment hours to 
+//event delegation to buttons to save text in local storage
 var saveButton = $(".saveBtn");
-//var buttonIcon = $(".fas")
 
 saveButton.on("click", function(){
 
     
-    var time = $(this).siblings().attr("id");
-    var textValue = $(this).siblings(".time-block").val();
-    var buttonIcon = $(this).children(".fas");
+    var time = $(this).siblings(".hour").text();
+    var textValue = $(this).siblings(".description").val();
+    
+    var buttonIcon = $(this).children(".fa-unlock");
 
     localStorage.setItem(time, textValue);
     buttonIcon.removeClass("fa-unlock");
     buttonIcon.addClass("fa-lock");
-    
+   
 });
+
+//event listener added to saveButtonOn to unsave text in local storage
+// saveButton.on("click", function(){
+//     localStorage.clear;
+//     textValue = "";
+//     var buttonIcon2 = $(this).children(".fa-lock");
+//     buttonIcon2.removeClass("fa-lock");
+//     buttonIcon2.addClass("fa-unlock");
+// });
+
+// retrieve data saved in local storage
+
+function retrieveData() {
+    var rows = $(".row");
+    rows.each(function () {
+     
+      var hour = $(this).find(".hour").text();
+     
+      var textValue = localStorage.getItem(hour);
+  
+      
+      if (textValue) {
+        $(this).find(".description").val(textValue);
+      }
+    });
+};
+
+//load data retrieving function when page loads
+$(document).ready(function () {
+    retrieveData();
+  });
