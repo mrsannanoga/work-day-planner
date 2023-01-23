a47ea25f5e2f05737c34b08e7ccc7f26c32b43f3
Ananas <mrs.anna.noga@gmail.com>
2023-01-23 00:28:05 +0000

    planner hours compared to current hour resulting in switching between classes of rows background color

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,67 @@
 //Set today's date displayed in header
 function updateNow() {
 
-var now = moment();
-$("#currentDay").text(now.format("dddd, D MMMM YYYY, h:mm:ss a"));
+    var now = moment();
+    $("#currentDay").text(now.format("dddd, D MMMM YYYY, h:mm:ss a"));
 
 };
 
 setInterval(updateNow);
 
-//set moment hours to 
+//set past, present and future filters to planner
+
+$(".row").each(function () {
+    var currentHour = moment().hour("HH");
+    var plannerHour = $(this).attr("id");
+
+    if (currentHour == plannerHour) {
+        $(this).addClass("present");
+
+    } else if (currentHour < plannerHour) {
+        $(this).removeClass("present");
+        $(this).addClass("future");
+
+    } else if (currentHour > plannerHour) {
+        $(this).removeClass("future");
+        $(this).addClass("past");
+    }
+});
+
+//set button icon class function
+
+
+// function buttonIconCLassSet(){
+//     var buttonIcon = saveButton.children(".fa-unlock");
+//     buttonIcon.removeClass("fa-unlock");
+//     buttonIcon.addClass("fa-lock");
+//  }; 
+
+
 //event delegation to buttons to save text in local storage
 var saveButton = $(".saveBtn");
 
-saveButton.on("click", function(){
+saveButton.on("click", function (event) {
 
-    
+
     var time = $(this).siblings(".hour").text();
     var textValue = $(this).siblings(".description").val();
-    
-    var buttonIcon = $(this).children(".fa-unlock");
+
+
 
     localStorage.setItem(time, textValue);
+
+    var buttonIcon = $(this).children(".fa-unlock");
     buttonIcon.removeClass("fa-unlock");
     buttonIcon.addClass("fa-lock");
-   
+
+    // function buttonIconCLassSet(event){
+    //     event.stopPropagation;
+    //     var buttonIcon = saveButton.children(".fa-unlock");
+    //     buttonIcon.removeClass("fa-unlock");
+    //     buttonIcon.addClass("fa-lock");
+    //  }; 
+    //buttonIconCLassSet($(".saveBtn"));
+
 });
 
 //event listener added to saveButtonOn to unsave text in local storage
@@ -40,19 +78,19 @@ saveButton.on("click", function(){
 function retrieveData() {
     var rows = $(".row");
     rows.each(function () {
-     
-      var hour = $(this).find(".hour").text();
-     
-      var textValue = localStorage.getItem(hour);
-  
-      
-      if (textValue) {
-        $(this).find(".description").val(textValue);
-      }
+
+        var hour = $(this).find(".hour").text();
+
+        var text = localStorage.getItem(hour);
+
+        if (text) {
+            $(this).find(".description").val(text);
+        }
+
     });
 };
 
 //load data retrieving function when page loads
 $(document).ready(function () {
     retrieveData();
-  });
+});
